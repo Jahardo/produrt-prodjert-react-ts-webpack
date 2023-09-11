3d6d985f6b37e0457059947272b7946cc8d520b2
Jahardo <dmorshch3@gmail.com>
2023-09-11 18:01:58 +0300

    add comment line over 100 sm ignore

--- src/shared/lib/ClassNemes.test.ts
+++ src/shared/lib/ClassNemes.test.ts
@@ -2,17 +2,33 @@ import { ClassNames } from './ClassNames';
 
 describe('ClassNames', () => {
     test('test with only parameter', () => {
-        expect(ClassNames('flex', {}, [])).toBe('flex');
+        expect(ClassNames('flex', {}, []))
+            .toBe('flex');
     });
     test('test with additional parameters', () => {
-        expect(ClassNames('flex', {}, ['flex1', 'flex2'])).toBe('flex flex1 flex2');
+        expect(ClassNames(
+            'flex',
+            {},
+            ['flex1', 'flex2'],
+        ))
+            .toBe('flex flex1 flex2');
     });
     test('test with mods parameters', () => {
         const expected = 'flex hovered scroll';
-        expect(ClassNames('flex', { hovered: true, scroll: true, focus: false }, [])).toBe(expected);
+        expect(ClassNames(
+            'flex',
+            { hovered: true, scroll: true, focus: false },
+            [],
+        ))
+            .toBe(expected);
     });
     test('test with mods undefined  parameters', () => {
         const expected = 'flex hovered scroll';
-        expect(ClassNames('flex', { hovered: true, scroll: true, focus: undefined }, [])).toBe(expected);
+        expect(ClassNames(
+            'flex',
+            { hovered: true, scroll: true, focus: undefined },
+            [],
+        ))
+            .toBe(expected);
     });
 });
